5fb391b472ef4aa1cfe173444c0690de0ad053d8
ObayIsm <obay.engineer@gmail.com>
2024-01-14 21:32:12 +0300

    Added alpha & beta pruning to enhance algorithm performance

--- PersisTheGame/ExpectiMiniMax.cs
+++ PersisTheGame/ExpectiMiniMax.cs
@@ -7,13 +7,16 @@ static class ExpectiMiniMax
         int bestValue = int.MinValue;
         PawnMovement? bestMove = null;
 
-        foreach(var move in node.GetPossibleMoves(Player.COMPUTER, shifts))
+        int alpha = int.MinValue;
+        int beta = int.MaxValue;
+
+        foreach (var move in node.GetPossibleMoves(Player.COMPUTER, shifts))
         {
             var child = new Node(node, move);
             List<int> newShifts = new List<int>(shifts);
             newShifts.Remove(move.Shift);
-            var value = Value(child, newShifts, true, 7);
-            if(value >= bestValue)
+            var value = Value(child, newShifts, true, 7, alpha, beta);
+            if (value >= bestValue)
             {
                 bestValue = value;
                 bestMove = move;
@@ -23,21 +26,21 @@ static class ExpectiMiniMax
         return bestMove!;
     }
 
-    public static int Value(Node node, List<int> shifts, bool isMaxPlayer, int depth)
+    public static int Value(Node node, List<int> shifts, bool maximizingPlayer, int depth, int alpha, int beta)
     {
         if (node.IsTerminal() || depth is 0)
             return node.Evaluate();
-        
-        if ( shifts.Count is 0 || node.GetPossibleMoves(isMaxPlayer ? Player.COMPUTER : Player.USER ,shifts).Count is 0 )
-            return ChanceValue(node, isMaxPlayer, depth);
-        
-        if (isMaxPlayer)
-            return MaxValue(node, shifts, isMaxPlayer, depth);
-        
-        return MinValue(node, shifts, isMaxPlayer, depth);
+
+        if (shifts.Count is 0 || node.GetPossibleMoves(maximizingPlayer ? Player.COMPUTER : Player.USER, shifts).Count is 0)
+            return ChanceValue(node, maximizingPlayer, depth, alpha, beta);
+
+        if (maximizingPlayer)
+            return MaxValue(node, shifts, maximizingPlayer, depth, alpha, beta);
+
+        return MinValue(node, shifts, maximizingPlayer, depth, alpha, beta);
     }
 
-    public static int MaxValue(Node node, List<int> shifts, bool isMaxPlayer, int depth)
+    public static int MaxValue(Node node, List<int> shifts, bool maximizingPlayer, int depth, int alpha, int beta)
     {
         var value = int.MinValue;
 
@@ -48,12 +51,17 @@ static class ExpectiMiniMax
             newShifts.Remove(move.Shift);
             var childNode = new Node(node, move);
             node.Children.Add(childNode);
-            value = Math.Max( value, Value( childNode, newShifts, isMaxPlayer, depth - 1) );
+            value = Math.Max(value, Value(childNode, newShifts, maximizingPlayer, depth - 1, alpha, beta));
+
+            alpha = Math.Max(alpha, value);
+            if (beta <= alpha)
+                break;
         }
+
         return value;
     }
 
-    public static int MinValue(Node node, List<int> shifts, bool isMaxPlayer, int depth)
+    public static int MinValue(Node node, List<int> shifts, bool maximizingPlayer, int depth, int alpha, int beta)
     {
         var value = int.MaxValue;
         var moves = node.GetPossibleMoves(Player.USER, shifts);
@@ -63,12 +71,16 @@ static class ExpectiMiniMax
             newShifts.Remove(move.Shift);
             var childNode = new Node(node, move);
             node.Children.Add(childNode);
-            value = Math.Min(value, Value(new Node(node, move), newShifts, isMaxPlayer, depth - 1));
+            value = Math.Min(value, Value(new Node(node, move), newShifts, maximizingPlayer, depth - 1, alpha, beta));
+
+            beta = Math.Min(beta, value);
+            if (beta <= alpha)
+                break;
         }
         return value;
     }
 
-    public static int ChanceValue(Node node, bool isMaxPlayer, int depth)
+    public static int ChanceValue(Node node, bool maximizingPlayer, int depth, int alpha, int beta)
     {
         double avg = 0;
         double noValidMovesNodesPossiblities = 0;
@@ -80,20 +92,33 @@ static class ExpectiMiniMax
             var shifts = Dice.GetShifts(i);
             var childNode = new Node(node, new PawnMovement(node.userPawns[0], 0));
             node.Children.Add(childNode);
-            
-            var ChildPossibleMovesCount = childNode.GetPossibleMoves(isMaxPlayer ? Player.COMPUTER : Player.USER, shifts).Count;
-            
+
+            var ChildPossibleMovesCount = childNode.GetPossibleMoves(maximizingPlayer ? Player.COMPUTER : Player.USER, shifts).Count;
+
             if (ChildPossibleMovesCount > 0 || !noValidMovesChildCalculated)
             {
-                var value = Value(childNode, shifts, !isMaxPlayer, depth - 1);
-                
+                var value = Value(childNode, shifts, !maximizingPlayer, depth - 1, alpha, beta);
+
                 if (ChildPossibleMovesCount > 0)
                     avg += possibilty * value;
-                
+
                 if (!noValidMovesChildCalculated)
                 {
                     noValidMovesNodesValue = value;
                     noValidMovesChildCalculated = true;
+                }
+
+                if (maximizingPlayer)
+                {
+                    alpha = Math.Max(alpha, value);
+                    if (beta <= alpha)
+                        break;
+                }
+                else
+                {
+                    beta = Math.Min(beta, value);
+                    if (beta <= alpha)
+                        break;
                 }
             }
             if (ChildPossibleMovesCount is 0) noValidMovesNodesPossiblities += possibilty;
